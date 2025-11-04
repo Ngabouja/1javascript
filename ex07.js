242277beb1a1ae3846c6e6a36239851f6a75883a
Ngabouja <ngabouja@edenschool.fr>
2025-11-04 15:24:54 +0100

    zeaf

--- ex07.js
+++ ex07.js
@@ -7,10 +7,10 @@ function getReduction(price, age) {
 	if (age <= 25 && age > 18) {
 		return price - price * 0.3;
 	}
-	if (age <= 18 && age > 5) {
+	if (age <= 18 && age >= 5) {
 		return price - price * 0.5;
 	}
-	if (age <= 5) {
+	if (age < 5) {
 		return 0;
 	}
 }
