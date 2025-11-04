93d998c007f1d046f3a34ecd3374dff1ef594fb9
Ngabouja <ngabouja@edenschool.fr>
2025-11-04 15:31:04 +0100

    ww

--- ex07.js
+++ ex07.js
@@ -1,18 +1,18 @@
 // ex07.js
 
 function getReduction(price, age) {
-	if (age > 25) {
-		return price;
-	}
-	if (age <= 25 && age > 18) {
-		return price - price * 0.3;
-	}
-	if (age <= 18 && age >= 5) {
-		return price - price * 0.5;
-	}
-	if (age < 5) {
-		return 0;
-	}
+    if (age > 25) {
+        return price;
+    }
+    if (age <= 25 && age > 18) {
+        return price - price * 0.3;
+    }
+    if (age <= 18 && age >= 5) {
+        return price - price * 0.5;
+    }
+    if (age < 5) {
+        return 0;
+    }
 }
 
 console.log(getReduction(10, 5)); // Résultat attendu: 0
